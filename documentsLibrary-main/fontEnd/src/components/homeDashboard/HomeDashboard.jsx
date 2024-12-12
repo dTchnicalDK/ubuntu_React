f5ac059d5dd0ece5c485a5fb7f4bd6284c4f62a6
dTchnicalDK <dtechnicaldk@gmail.com>
2024-12-12 13:57:00 +0530

    nav bar css done, yet miles to go

--- documentsLibrary-main/fontEnd/src/components/homeDashboard/HomeDashboard.jsx
+++ documentsLibrary-main/fontEnd/src/components/homeDashboard/HomeDashboard.jsx
@@ -3,6 +3,9 @@ import "./HomeDashboard.css";
 import { HiOutlineSearch } from "react-icons/hi";
 import { CiBarcode } from "react-icons/ci";
 import { MdGTranslate } from "react-icons/md";
+import { IoMdClock } from "react-icons/io";
+import { SlCalender } from "react-icons/sl";
+import { FaCaretDown } from "react-icons/fa";
 
 function HomeDashboard() {
   return (
@@ -28,6 +31,7 @@ function HomeDashboard() {
                   <button>
                     <HiOutlineSearch />
                   </button>
+                  <div className="line"></div>
                   <button>
                     <CiBarcode />
                   </button>
@@ -47,8 +51,30 @@ function HomeDashboard() {
                 <option value="lan5">lan5</option>
               </select>
             </div>
-            <div className="button dateNtime"></div>
-            <div className="button user"></div>
+            <div className="button dateNtime eleDiv">
+              <div className="time">
+                <button>
+                  <IoMdClock />
+                </button>
+                <span>09:00:am</span>
+              </div>
+              <div className="date">
+                <button>
+                  <SlCalender />
+                </button>
+                <span>05/feb/1984</span>
+              </div>
+            </div>
+            <div className="button user eleDiv">
+              <div>
+                <img src="../src/assets/images/profileImg.png" alt="photo" />
+              </div>
+
+              <label>unknown</label>
+              <button>
+                <FaCaretDown />
+              </button>
+            </div>
           </div>
           <div className="main"></div>
         </div>
