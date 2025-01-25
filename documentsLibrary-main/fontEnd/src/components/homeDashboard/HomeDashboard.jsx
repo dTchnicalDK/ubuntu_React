98dd693c491de4d458fa2161df7bd171c7b6b0ec
dTchnicalDK <dtechnicaldk@gmail.com>
2025-01-25 13:29:12 +0530

    see all button in each divsions working now

--- documentsLibrary-main/fontEnd/src/components/homeDashboard/HomeDashboard.jsx
+++ documentsLibrary-main/fontEnd/src/components/homeDashboard/HomeDashboard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from "react";
 import "./HomeDashboard.css";
 import DashboardNav from "./DashboardNav/DashboardNav";
 import LeftSidebar from "./leftSidebar/LeftSidebar";
@@ -7,6 +8,22 @@ import NewArrival from "./NewArrival/NewArrival";
 import BookCard from "./booksCard/BookCard";
 
 function HomeDashboard() {
+  const divisionRef = useRef(null); //refrence for division div;
+  const recetReadingRef = useRef(null);
+
+  const handleExpand = () => {
+    console.log("expand clicked");
+    if (divisionRef.current) {
+      divisionRef.current.classList.toggle("expanded");
+    }
+  };
+  const expandRecentReadings = () => {
+    console.log("recent reading clicked");
+    if (recetReadingRef.current) {
+      recetReadingRef.current.classList.toggle("expanded");
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -22,28 +39,86 @@ function HomeDashboard() {
           </div>
           <div className="main-lower">
             <h2>Good Morning</h2>
-            <div className="recommended divisions">
+            <div className="recommended divisions " ref={divisionRef}>
               <div className="div-details">
                 <h4>Recommended for you</h4>
-                <h5>see all</h5>
+                <h5 onClick={handleExpand}>see all</h5>
               </div>
               <div className="div-container">
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
+                <BookCard
+                  bookData={{
+                    title: "The Book Of Art",
+                    writer: "Dharmendra",
+                    cover: "/images/theBookOfArt.jpg",
+                    year: "5555",
+                    rating: "8",
+                  }}
+                />
+                <BookCard
+                  bookData={{
+                    title: "Walk into the shadow",
+                    cover: "/images/walkInto.jpeg",
+                    year: "5555",
+                    rating: "100",
+                  }}
+                />
+                <BookCard
+                  bookData={{
+                    title: "title",
+                    cover: "/images/harryPotter.png",
+                    year: "5555",
+                    rating: "100",
+                  }}
+                />
+                <BookCard
+                  bookData={{
+                    title: "title",
+                    cover: "/images/harryPotter.png",
+                    year: "5555",
+                    rating: "100",
+                  }}
+                />
+                <BookCard
+                  bookData={{
+                    title: "title",
+                    cover: "/images/harryPotter.png",
+                    year: "5555",
+                    rating: "100",
+                  }}
+                />
+                <BookCard
+                  bookData={{
+                    title: "title",
+                    cover: "/images/harryPotter.png",
+                    year: "5555",
+                    rating: "100",
+                  }}
+                />
+                <BookCard
+                  bookData={{
+                    title: "title",
+                    cover: "/images/harryPotter.png",
+                    year: "5555",
+                    rating: "100",
+                  }}
+                />
                 <BookCard />
                 <BookCard />
                 <BookCard />
               </div>
             </div>
-            <div className="recent-reading divisions">
+            <div className="recent-reading divisions" ref={recetReadingRef}>
               <div className="div-details">
                 <h4>Recent Readings</h4>
-                <h5>see all</h5>
+                <h5 onClick={expandRecentReadings}>see all</h5>
               </div>
 
               <div className="div-container">
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
                 <BookCard />
                 <BookCard />
                 <BookCard />
--- documentsLibrary-main/fontEnd/src/components/homeDashboard/booksCard/BookCard.jsx
+++ documentsLibrary-main/fontEnd/src/components/homeDashboard/booksCard/BookCard.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import "./BookCard.css";
 
-function BookCard() {
+function BookCard({
+  bookData = {
+    title: "dummy tittle",
+    writer: "jonathan swift,",
+    cover: "/images/harryPotter.png",
+    year: 9999,
+    rating: 10,
+  },
+}) {
+  const { title, writer, cover, year, rating } = bookData;
+  // console.log(title);
+
   return (
     <div className="book-div">
       <div className="cover-page">
-        <img src="./harryPotter.png" alt="book-cover" />
+        <img src={cover} alt="book-cover" />
       </div>
       <div className="details">
-        <h3>The Gulliver's travel</h3>
+        <h3>{title}</h3>
         <div className="sub-details">
-          <span> jonathan swift, </span>
-          <span>1983</span>
+          <span> {writer} </span>
+          <span>{year}</span>
         </div>
       </div>
-      <small>4/10</small>
+      <small>{rating}/10</small>
     </div>
   );
 }
